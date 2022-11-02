ff8373009e10800a847f1647e847a69408a56eb6
Madhav Ramesh <madhavramesh02@gmail.com>
2022-11-01 22:56:40 -0400

    Trying to add bonsai

---
+++ third_party/bonsai.h
@@ -0,0 +1,36 @@
+#ifndef _BONSAI_H_
+#define _BONSAI_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+#include <netinet/if_ether.h>
+#include <netinet/in.h>
+
+#define HOST_MAX_LENGTH 256 // RFC 2181
+
+typedef struct lnxbody_s {
+	char remote_phys_host[HOST_MAX_LENGTH]; 
+	uint16_t remote_phys_port;
+	struct in_addr local_virt_ip; // Network-byte-order
+	struct in_addr remote_virt_ip; // Network-byte-order
+
+	struct lnxbody_s *next;
+} lnxbody_t;
+
+typedef struct {
+	uint16_t local_phys_port;
+	lnxbody_t* body;
+} lnxinfo_t;
+
+// Returns a lnxinfo_t struct. Call free_links to clean up
+lnxinfo_t *parse_links(char *filename);
+void free_links(lnxinfo_t *lnx);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
